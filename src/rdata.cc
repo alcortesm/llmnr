4e76b0c4e207a538c98b5d785f9b347115248a7d
Alberto Cortes Martin <alcortes@hagrid.(none)>
2008-06-12 15:56:48 +0200

    Added new class Rdata
    It will be the super class of RdataA, RdataMX...

---
+++ src/rdata.cc
@@ -0,0 +1,9 @@
+#include "rdata.h"
+
+using rr::Rdata;
+
+unsigned short
+rr::Rdata::length()
+{
+    return d_length;
+}
---
+++ src/rdata.h
@@ -0,0 +1,14 @@
+// 
+
+#ifndef INCLUDED_RDATA
+#define INCLUDED_RDATA
+
+namespace rr {
+
+    class Rdata {
+        unsigned short d_length;
+    public:
+        unsigned short length();
+    };
+}
+#endif
