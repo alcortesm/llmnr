66cd010e99254a2d51671c508d74933d5d96a875
Alberto Cortes <alcortes@it.uc3m.es>
2008-07-24 10:49:45 +0200

    Added argument -v to test.sh for valgrind check

--- src/test.sh
+++ src/test.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-valgrind --tool=memcheck --leak-check=full --show-reachable=yes -q ./test
+if [ $# -gt 1 ]
+then
+    echo "test.sh [-v]" > /dev/stderr
+    exit 1
+fi
 
+if [ $# -eq 0 ]
+then
+    ./test
+    exit $?
+fi
+
+if [ $1 == "-v" ]
+then
+    valgrind --tool=memcheck --leak-check=full --show-reachable=yes -q ./test
+else
+    echo "test.sh [-v]" > /dev/stderr
+    exit 1
+fi
