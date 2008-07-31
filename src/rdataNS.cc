0457907c45bee0c4c9ba6b6b1b6af100cf537911
Alberto Cortes <alcortes@it.uc3m.es>
2008-07-31 14:57:40 +0200

    Changed marshall and unmarshall of rdataNS to use the new util::dnsname2buf and util::buf2dnsname

--- src/rdataNS.cc
+++ src/rdataNS.cc
@@ -65,80 +65,24 @@ RdataNS::printOn(std::ostream & s) const
 void
 RdataNS::marshall(char * & offset) const
 {
-    string s = this->nsdname();
-    char const * ca = s.c_str();
-    size_t dot;
-    size_t label = 0;
-    size_t label_sz;
-    size_t len = s.length();
-
-    for (;;) { // for each label...
-        dot = s.find_first_of('.', label);
-        if (dot == string::npos) { // this is the last label
-            label_sz = len - label;
-        } else {
-            label_sz = dot - label;
-        }
-
-        // write label size
-        *offset = (char) label_sz;
-        offset++;
-        
-        // write label
-        memcpy(offset, ca+label, label_sz);
-        offset += label_sz;
-        
-        label += label_sz + 1 ; // label size plus the dot
-        if (label > len) { // this was the last label
-            *offset = '\0';
-            offset++;
-            break;
-        }
-    }
-
+    util::dnsname2buf(this->nsdname(), offset);
     return;
 }
 
 RdataNS const *
 RdataNS::unmarshall(char const * & offset) throw (Rdata::ExBadSyntax)
 {
-    size_t label_sz;
-    char * const s = (char * const) malloc((util::MAX_DNAME_SIZE+1)*sizeof(char));
-    if (!s) {
-        std::cerr << "Out of memory!" << std::endl ;
-        exit (EXIT_FAILURE);
-    }
-    char * os = s;
-    for (;;) { // for each label...
-        
-        // read label_sz
-        label_sz = (size_t) *offset;
-        offset++;
-
-        // if end of data change the last dot for a \0 and finish
-        if (label_sz == 0) {
-            os --;
-            *os = '\0';
-            break;
-        }
-
-        // copy label
-        memcpy(os, offset, label_sz);
-        offset += label_sz;
-        os += label_sz;
-        if (os-s > util::MAX_DNAME_SIZE+1)
-            throw Rdata::ExBadSyntax();
-
-        // write extra dot
-        *os = '.';
-        os++;
-        if ((os-s) > util::MAX_DNAME_SIZE+1)
-            throw Rdata::ExBadSyntax();
+    string * sp;
+    try {
+        sp = util::buf2dnsname(offset);
+    } catch (string & ex) {
+        throw Rdata::ExBadSyntax();
     }
 
     RdataNS const * dp;
-    dp = RdataNS::parse(s);
-    free(s);
+    dp = RdataNS::parse(*sp);
+    
+    delete sp;
     return dp;
 }
 
